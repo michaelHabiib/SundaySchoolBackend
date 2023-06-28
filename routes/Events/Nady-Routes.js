961509866bf808189533b770b47fafd03aada123
Michael Ramzy <ramzymika@gmail.com>
2023-06-28 13:51:59 +0300

    28-06 commit Update auth for Nady Reservtion

--- routes/Events/Nady-Routes.js
+++ routes/Events/Nady-Routes.js
@@ -3,7 +3,7 @@ import {AddNewBook, GetAllNadyRes} from '../../Controlles/Events/Nady-controlles
 import { verfiyUser, verfiyIsAdmin, VerfiychoiceNady} from "../../middelwares/VerfiyAuth";
 const NadyRoutes = express.Router();
 
-NadyRoutes.post('/nady', verfiyUser, VerfiychoiceNady, AddNewBook)
+NadyRoutes.post('/nady', verfiyUser, AddNewBook)
 NadyRoutes.get('/nady', verfiyIsAdmin, GetAllNadyRes)
 // NadyRoutes.get('/funday/downloads', verfiyIsAdmin, GetAllFundayResExcel)
 
