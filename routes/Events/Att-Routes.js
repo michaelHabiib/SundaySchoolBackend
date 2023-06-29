8b541148f928c2d23335a7e8faccf1e180638088
Michael Ramzy <ramzymika@gmail.com>
2023-06-29 09:42:24 +0300

    29-06 commit remove Admin Auth from Save Att

--- routes/Events/Att-Routes.js
+++ routes/Events/Att-Routes.js
@@ -3,7 +3,7 @@ import { SaveAtt, GetAttOfaDay } from "../../Controlles/Events/Att-controlles";
 import { verfiyIsAdmin } from "../../middelwares/VerfiyAuth";
 const attRoutes = express.Router();
 
-attRoutes.post('/', verfiyIsAdmin, SaveAtt)
+attRoutes.post('/', SaveAtt)
 attRoutes.get('/:day', verfiyIsAdmin, GetAttOfaDay)
 
 export default attRoutes;
