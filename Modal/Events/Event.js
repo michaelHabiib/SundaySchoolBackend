ebec4e5a922c9e7c425180e5ed542e591b48567a
Michael Ramzy <ramzymika@gmail.com>
2023-07-24 14:41:19 +0300

    24-07-2023 update to add color in Event Schema

--- Modal/Events/Event.js
+++ Modal/Events/Event.js
@@ -19,11 +19,7 @@ const EventSchema = new schema({
         type : Number,
         required : [true, 'Event price is Required']
     },
-    color : [{
-      color : {
-        type : String,
-      }
-    }],
+    color : [],
     avaliableDates : [{
         date : {
             type : String,
