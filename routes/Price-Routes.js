9b4a53cad10bd99565671964d0428816250e6d41
Michael Ramzy <ramzymika@gmail.com>
2023-09-07 11:57:38 +0300

    07-09-2023

--- routes/Price-Routes.js
+++ routes/Price-Routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { AddNewPricePlan, GetALLPricesPlans, DeletePricePlan } from "../Controlles/Price-controlles";
+import { AddNewPricePlan, GetALLPricesPlans, DeletePricePlan, UpdatePrice } from "../Controlles/Price-controlles";
 const PriceRoutes = express.Router();
 
 PriceRoutes.post('/',AddNewPricePlan)
 PriceRoutes.get('/plans',GetALLPricesPlans)
 PriceRoutes.delete('/plans/:id',DeletePricePlan)
-// PriceRoutes.post('/blogs/:id',UpdateBlog)
+PriceRoutes.post('/plans/:id',UpdatePrice)
 export default PriceRoutes;
