9209b5617db2a5317fa39729667430492a09e387
Michael Ramzy <ramzymika@gmail.com>
2023-09-07 10:54:35 +0300

    07-09-2023

--- Controlles/Price-controlles.js
+++ Controlles/Price-controlles.js
@@ -23,3 +23,46 @@ export const GetALLPricesPlans = async (req,res,next) => {
         console.log(error);
     }
 }
+export const DeletePricePlan = async (req, res, next) => {
+    const id = req.params.id
+    try {
+        console.log(id);
+        const deletedPlan = await Price.findByIdAndRemove(id)
+        if(!deletedPlan){
+            return res.status(404).json({message : 'No Blog With this ID'})
+        }
+        return res.status(200).json({message : 'Deleted Sucssfully', deletedPlan})
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const UpdatePrice = async (req, res, next) => {
+    const {         
+        planTitle,
+        PlanPrice,
+        PlanLength,
+        featuers} = req.body;
+    const id = req.params.id
+  
+    try {
+        console.log(id);
+      const plan = await Price.findById({_id : id});
+        console.log(plan);
+      if (!plan) {
+        return res.status(404).json({ message: 'Price Plan not found' });
+      }
+  
+      // Update Blog properties
+      plan.planTitle = planTitle;
+      plan.PlanPrice = PlanPrice;
+      plan.PlanLength = PlanLength;
+      plan.featuers = featuers;
+  
+      await plan.save();
+  
+      return res.status(200).json({ message: 'Price Plan updated successfully' });
+    } catch (error) {
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  };
--- routes/Price-Routes.js
+++ routes/Price-Routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { AddNewPricePlan, GetALLPricesPlans } from "../Controlles/Price-controlles";
+import { AddNewPricePlan, GetALLPricesPlans, DeletePricePlan } from "../Controlles/Price-controlles";
 const PriceRoutes = express.Router();
 
 PriceRoutes.post('/',AddNewPricePlan)
 PriceRoutes.get('/plans',GetALLPricesPlans)
-// PriceRoutes.delete('/blogs/:id',DeleteBlog)
+PriceRoutes.delete('/plans/:id',DeletePricePlan)
 // PriceRoutes.post('/blogs/:id',UpdateBlog)
 export default PriceRoutes;
